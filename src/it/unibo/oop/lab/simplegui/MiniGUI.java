c71ca6d775a3dc9e4994c446f4d251f04de86242
federico.campanozzi@studio.unibo.it <federico.campanozzi@studio.unibo.it>
2020-11-16 11:46:38 +0100

    Finito es 1

--- src/it/unibo/oop/lab/simplegui/MiniGUI.java
+++ src/it/unibo/oop/lab/simplegui/MiniGUI.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package it.unibo.oop.lab.simplegui;
 
 import java.awt.BorderLayout;
@@ -10,8 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -33,18 +32,28 @@ public class MiniGUI {
      */
     public MiniGUI() {
         final JPanel canvas = new JPanel();
+        final JPanel canvasInner = new JPanel();
         canvas.setLayout(new BorderLayout());
+        canvasInner.setLayout(new BoxLayout(canvasInner, BoxLayout.X_AXIS));
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JLabel result = new JLabel("   Result :   ");
+        final JLabel lb = new JLabel("Random Int  ");
+        canvas.add(canvasInner, BorderLayout.CENTER);
+        canvasInner.add(write, BorderLayout.CENTER);
+        canvas.add(result, BorderLayout.NORTH);
+        canvas.add(lb, BorderLayout.NORTH);
         /*
          * Handlers
          */
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(rng.nextInt());
+                final int randomInt = rng.nextInt();
+                lb.setText(Integer.toString(randomInt));
+                System.out.println(randomInt);
+                // frame.pack();
             }
         });
     }
@@ -52,33 +61,35 @@ public class MiniGUI {
     private void display() {
         /*
          * Make the frame one fifth the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected. In order to deal coherently with multimonitor
-         * setups, other facilities exist (see the Java documentation about this
-         * issue). It is MUCH better than manually specify the size of a window
-         * in pixel: it takes into account the current resolution.
+         * enough for a single screen setup. In case of multiple monitors, the primary
+         * is selected. In order to deal coherently with multimonitor setups, other
+         * facilities exist (see the Java documentation about this issue). It is MUCH
+         * better than manually specify the size of a window in pixel: it takes into
+         * account the current resolution.
          */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / PROPORTION, sh / PROPORTION);
         /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
         /*
          * OK, ready to pull the frame onscreen
          */
         frame.setVisible(true);
+        frame.pack();
     }
 
     /**
-     * @param args ignored
+     * @param args
+     *                 ignored
      */
     public static void main(final String... args) {
-       new MiniGUI().display();
+        new MiniGUI().display();
     }
 
 }
